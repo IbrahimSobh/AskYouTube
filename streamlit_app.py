3d82f985944f01c863547f45873edcb80e70e545
Ibrahim Sobh <im_sobh@hotmail.com>
2023-07-23 19:19:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,7 @@ with st.form('myform', clear_on_submit=True):
     google_api_key = st.text_input('Google PaLM🌴 API Key', type='password', disabled=not (youtube_url and query_text))
     submitted = st.form_submit_button('Submit', disabled=not(youtube_url and query_text))
     if submitted and google_api_key:
-        with st.spinner('Calculating...'):
+        with st.spinner('Calculating (his may take a couple of minutes depending on the video) ...'):
             response = generate_response(youtube_url, google_api_key, query_text)
             if response:
                 result.append(response)
